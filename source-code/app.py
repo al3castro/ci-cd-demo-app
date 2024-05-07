eaf08e39104e8676d6760d28cc896ff3b52d4d7d
Alejandro Castro <alejandro.castro.afonso@proton.me>
2024-05-07 14:06:40 +0100

    test 1

--- source-code/app.py
+++ source-code/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '¡Hola, mundo! Este es un servidor web simple con Flask en Python.'
+    return '¡Hola, mundo!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
